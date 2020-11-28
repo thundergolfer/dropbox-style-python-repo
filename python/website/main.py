5f89677a3654e3631b819c0657acd522e8310f9d
Jonathon Belotti <jonathon@canva.com>
2020-11-28 12:51:50 +0000

    Tweaks to main.py to get it working

--- python/website/main.py
+++ python/website/main.py
@@ -9,16 +9,18 @@ def hello_app(environ, start_response):
     start_response('200 OK', [('Content-type', 'text/html')])
 
     return [
-        '<html><strong>',
-        say_hello(),
-        '</strong></html>',
+        b'<html><strong>',
+        say_hello().encode(),
+        b'</strong></html>',
     ]
 
-
 def main(port):
-    server = make_server('localhost', port, hello_app)
-    server.serve_forever()
+    with make_server('', port, hello_app) as httpd:
+        print(f"Serving HTTP on port {port}...")
+
+        # Respond to requests until process is killed
+        httpd.serve_forever()
 
 
 if __name__ == '__main__':
-    main(int(sys.argv[1]))
+    main(port=int(sys.argv[1]))
